651dd50875706decde689856b787b8b3b6c24803
Maxly Garcia <maxgarcia6@gmail.com>
2016-03-15 21:43:13 -0400

    Otra linea

--- HelloCSharp/HelloCSharp/Program.cs
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -13,6 +13,7 @@ namespace HelloCSharp
             Console.WriteLine("Hello, CSharp"); // Imprime en la consola.
             Console.ReadKey(); // Espera que se presione una tecla para cerrar.
             Console.WriteLine("Stinger was here"); // Mi comment
+            Console.WriteLine("Stinger was here"); // otro comment
         }
     }
 }
